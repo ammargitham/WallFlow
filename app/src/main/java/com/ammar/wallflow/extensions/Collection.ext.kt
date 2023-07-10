8a896ee08546cec314e0f759fe1211f918b525a3
Ammar Githam <ammar.githam@outlook.com>
2023-07-10 20:50:08 +0900

    Remove unused code

--- app/src/main/java/com/ammar/wallflow/extensions/Collection.ext.kt
+++ app/src/main/java/com/ammar/wallflow/extensions/Collection.ext.kt
@@ -8,5 +8,3 @@ fun <E> List<E>.randomList(size: Int): List<E> = this.asSequence()
 fun Map<String, String>.toQueryString(): String = this.entries.joinToString("&") { (k, v) ->
     "${k.urlEncoded()}=${v.urlEncoded()}"
 }
-
-fun <E> Collection<E>?.isNotNullOrEmpty() = this?.isNotEmpty() ?: false
--- app/src/main/java/com/ammar/wallflow/extensions/Compose.ext.kt
+++ app/src/main/java/com/ammar/wallflow/extensions/Compose.ext.kt
@@ -1,13 +1,10 @@
 package com.ammar.wallflow.extensions
 
-import android.util.Log
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.layout.layout
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntSize
@@ -23,30 +20,6 @@ fun Dp.toPx() = with(LocalDensity.current) { roundToPx() }
 
 @Composable
 fun Dp.toPxF() = with(LocalDensity.current) { toPx() }
-
-fun Modifier.ignoreHorizontalParentPadding(horizontal: Dp) =
-    this.layout { measurable, constraints ->
-        val overriddenWidth = constraints.maxWidth + 2 * horizontal.roundToPx()
-        val placeable = measurable.measure(constraints.copy(maxWidth = overriddenWidth))
-        layout(placeable.width, placeable.height) {
-            placeable.place(0, 0)
-        }
-    }
-
-fun Modifier.ignoreVerticalParentPadding(vertical: Dp, oneSide: Boolean = false) =
-    this.layout { measurable, constraints ->
-        val addingHeight = (if (oneSide) 1 else 2) * vertical.roundToPx()
-        val overriddenHeight = constraints.maxHeight + addingHeight
-        val newConstraints = constraints.copy(maxHeight = overriddenHeight)
-        Log.d(
-            TAG,
-            "ignoreVerticalParentPadding: before: ${constraints.maxHeight}, adding: $addingHeight, after: ${newConstraints.maxHeight}"
-        )
-        val placeable = measurable.measure(newConstraints)
-        layout(placeable.width, placeable.height) {
-            placeable.place(0, 0)
-        }
-    }
 
 val IntSize.aspectRatio
     get() = if (height != 0) width.toFloat() / height else 0F
