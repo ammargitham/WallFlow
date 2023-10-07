6b588f3f751f3fd763e1636ee5a58e802c206869
Ammar Githam <ammar.githam@outlook.com>
2023-10-07 20:58:51 +0900

    Fix import order

--- app/src/main/java/com/ammar/wallflow/data/network/model/serializers/NetworkWallhavenMetaQuerySerializer.kt
+++ app/src/main/java/com/ammar/wallflow/data/network/model/serializers/NetworkWallhavenMetaQuerySerializer.kt
@@ -1,9 +1,9 @@
 package com.ammar.wallflow.data.network.model.serializers
 
-import kotlinx.serialization.json.JsonContentPolymorphicSerializer as PolymorphicSerializer
 import com.ammar.wallflow.data.network.model.wallhaven.NetworkWallhavenMetaQuery
 import com.ammar.wallflow.data.network.model.wallhaven.StringNetworkWallhavenMetaQuery
 import com.ammar.wallflow.data.network.model.wallhaven.TagNetworkWallhavenMetaQuery
+import kotlinx.serialization.json.JsonContentPolymorphicSerializer as PolymorphicSerializer
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonPrimitive
 
