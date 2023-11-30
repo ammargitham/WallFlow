714adf9e7f00ab9a40528bc6525314a27698c835
Ammar Githam <ammar.githam@outlook.com>
2023-11-30 22:24:56 +0900

    Release 2.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.ammar.wallflow"
         minSdk = 24
         targetSdk = 34
-        versionCode = 12
-        versionName = "2.1.0"
+        versionCode = 13
+        versionName = "2.2.0"
 
         val abi = getAbi()
         ndk {
