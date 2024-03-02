28f7fbec37fcbdf852995a4a1534271d37ff3192
Ammar Githam <ammar.githam@outlook.com>
2024-03-02 17:21:39 +0900

    Make MoreScreen scrollable

--- app/src/main/java/com/ammar/wallflow/ui/screens/more/MoreListContainer.kt
+++ app/src/main/java/com/ammar/wallflow/ui/screens/more/MoreListContainer.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
@@ -37,7 +39,9 @@ fun MoreListContainer(
     onItemClick: (MoreListItem.Clickable) -> Unit = {},
 ) {
     PermanentDrawerSheet(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(state = rememberScrollState()),
     ) {
         items.forEach { item ->
             when (item) {
