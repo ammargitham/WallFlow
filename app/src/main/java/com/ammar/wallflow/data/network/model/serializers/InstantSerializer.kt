993371ea15c69280ac2f91a977703080d20f802c
Ammar Githam <ammar.githam@outlook.com>
2024-03-02 19:14:46 +0900

    Upgrade kotlinx-datetime

--- app/src/main/java/com/ammar/wallflow/data/network/model/serializers/InstantSerializer.kt
+++ app/src/main/java/com/ammar/wallflow/data/network/model/serializers/InstantSerializer.kt
@@ -1,7 +1,6 @@
 package com.ammar.wallflow.data.network.model.serializers
 
 import kotlinx.datetime.Instant
-import kotlinx.datetime.toInstant
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
@@ -10,14 +9,16 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
 object InstantSerializer : KSerializer<Instant> {
-    override fun deserialize(decoder: Decoder) =
-        (decoder.decodeString().replace(" ", "T") + "Z").toInstant()
+    override fun deserialize(decoder: Decoder) = Instant.parse(
+        decoder.decodeString().replace(" ", "T") + "Z",
+    )
 
     override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor(
         serialName = "Instant",
         kind = PrimitiveKind.STRING,
     )
 
-    override fun serialize(encoder: Encoder, value: Instant) =
-        encoder.encodeString(value.toString())
+    override fun serialize(encoder: Encoder, value: Instant) = encoder.encodeString(
+        value.toString(),
+    )
 }
