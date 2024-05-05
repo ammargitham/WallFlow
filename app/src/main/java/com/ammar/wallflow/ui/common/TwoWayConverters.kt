387fbbb9a51a3e48ff60daab3ab19ab592e151ce
Ammar Githam <ammar.githam@outlook.com>
2024-05-05 17:59:29 +0900

    Prevent rare crash due to negative padding

--- app/src/main/java/com/ammar/wallflow/ui/common/TwoWayConverters.kt
+++ app/src/main/java/com/ammar/wallflow/ui/common/TwoWayConverters.kt
@@ -22,10 +22,10 @@ fun getPaddingValuesConverter(layoutDirection: LayoutDirection) =
         },
         convertFromVector = { vector: AnimationVector4D ->
             PaddingValues(
-                top = vector.v1.dp,
-                bottom = vector.v2.dp,
-                start = vector.v3.dp,
-                end = vector.v4.dp,
+                top = vector.v1.coerceAtLeast(0f).dp,
+                bottom = vector.v2.coerceAtLeast(0f).dp,
+                start = vector.v3.coerceAtLeast(0f).dp,
+                end = vector.v4.coerceAtLeast(0f).dp,
             )
         },
     )
